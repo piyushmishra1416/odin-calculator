93f33436c77c28b5cc85d4f48dee613b92226acf
Piyush <piyushmishra1416@gmail.com>
2023-03-12 14:55:51 +0530

    made basic outline for calci using html and css

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(input1 , input2){
+   sum= input1+input2;
+}
+function subtract(input1, input2) {
+   diff= input1-input2;
+}
+function multiply(input1, input2){
+   mul= input1*input2;
+}
+
+function divide(input1, input2){
+   div = input1 / input2;
+}
+function operate(input1, input2, operator){
+   input1 = parseInt(alert("first No"));
+   input2 = parseInt(alert("second No"));
+   operator = alert ("operator");
+   
+}
